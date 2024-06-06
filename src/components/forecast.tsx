866790c9d0c6d23426d3fc4f61e144d357888093
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-06-06 19:57:33 -0300

    forecast days

--- src/components/forecast.tsx
+++ src/components/forecast.tsx
@@ -1,8 +1,11 @@
-export default function Forecast({city}:any)
+import { setIcon } from "./weatherlogo"
+
+export default function Forecast({city, day}:any)
 {
-    return(
+    
+    return(      
         <div className="w-full h-12 flex transition-colors items-center justify-between rounded-lg border border-transparent hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-            <p>{city}</p>     
+            <p className="pl-3">{city}</p>     
         </div>
     )
 }
