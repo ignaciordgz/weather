26647b915e0f081dcef439ab265ebae94bd3bdd0
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-06-07 21:49:14 -0300

    fix

--- src/components/forecast.tsx
+++ src/components/forecast.tsx
@@ -5,8 +5,16 @@ import { setIcon } from "./weatherlogo"
 
 export default function Forecast({city, day}:any)
 {
-    
-    var icon = setIcon(day.main)
+    var icon;
+
+    if (day == undefined)
+    {
+        day = { temp : 0, humidity: 0, wind: 0, main: "Clear"}
+    }
+    else
+    {
+        icon = setIcon(day.main)
+    }
 
     return(      
         <div className="w-full h-12 flex transition-colors items-center justify-between rounded-lg border border-transparent hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
@@ -16,12 +24,15 @@ export default function Forecast({city, day}:any)
                     <p className="transition opacity-45 font-medium">{day.temp + "°C"}</p>
                 </div>
                 <div className="pr-2">
-                    <Image 
+                {icon && (  
+                    <Image
                     src={icon}
-                    alt="day main weather"
+                    alt="weather day"
                     width={40}
                     height={40}
+                    property=""
                     />
+                )}
                 </div> 
             </div>    
         </div>
