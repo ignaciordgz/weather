dd1814fffbb6619dc46528d85a7c990064b71736
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-06-07 21:59:56 -0300

    humidity days

--- src/components/forecast.tsx
+++ src/components/forecast.tsx
@@ -17,11 +17,15 @@ export default function Forecast({city, day}:any)
     }
 
     return(      
-        <div className="w-full h-12 flex transition-colors items-center justify-between rounded-lg border border-transparent hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <div className="w-full h-12 group flex transition-colors items-center justify-between rounded-lg border border-transparent hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
             <p className="pl-3">{city}</p>
-            <div className="flex justify-between w-32">
+            <div className="flex justify-between w-52 items-center">
+                <div>
+                    <Humidity className="opacity-45 group-hover:opacity-95 transition-all"/>
+                    <p className="group-hover:opacity-95 transition opacity-45 font-light text-sm">{day.humidity + "%"}</p>
+                </div>
                 <div className="items-center inline-flex">
-                    <p className="transition opacity-45 font-medium">{day.temp + "°C"}</p>
+                    <p className="group-hover:opacity-100 transition opacity-75 font-medium">{day.temp + "°C"}</p>
                 </div>
                 <div className="pr-2">
                 {icon && (  
