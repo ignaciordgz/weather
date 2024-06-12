26a54eb8046ce90884e24cd7f3b88eb7eff91494
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-06-12 20:16:42 -0300

    location button

--- src/components/location.tsx
+++ src/components/location.tsx
@@ -1,10 +1,14 @@
 "use client"
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function LocationButton()
 {
     return(
-        <div className="w-10 h-9 rounded-md transition hover:bg-gray-800">
-            <button className="w-10 h-9">L</button>
+        <div className="w-10 h-9 group rounded-md transition-colors hover:dark:bg-neutral-600/30">
+            <button className="w-10 h-9">
+                <FontAwesomeIcon icon={faLocationDot} className="opacity-65 transition group-hover:opacity-100"/>
+            </button>
         </div>
     )
 }
