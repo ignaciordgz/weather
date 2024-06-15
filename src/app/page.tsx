bca5fe24f74892326bcf454729018adfe3902b40
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-06-14 21:04:36 -0300

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import MainScreen from "@/pages/screen";
 
 export default function Home() {
   return (
+    <div>
       <MainScreen/>
+      <footer>Made with Next.js</footer>
+    </div>
   );
 }
