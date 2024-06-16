01be56e30c31c4bb1aba148acfc154ef28cdb3ac
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-06-15 23:46:45 -0300

    footer fix

---
+++ src/components/footer.tsx
@@ -0,0 +1,18 @@
+import { Arrow } from "./svg/arrow"
+
+export default function Footer()
+{
+    return(
+        <div>
+            <footer className="py-4 w-full">
+                <div className="flex items-center">
+                    <div className="group">
+                        <a className="flex pl-3 items-center opacity-40 group-hover:opacity-85 transition text-sm font-medium" target="_blank" href="https://www.github.com/ignaciordgz">Made by ignaciordgz with Next.js
+                            <Arrow className="w-4 h-4 group-hover:opacity-85 transition"></Arrow>
+                        </a>
+                    </div>
+                </div>
+            </footer>
+        </div>        
+    )
+}
---
+++ src/components/svg/arrow.tsx
@@ -0,0 +1,1 @@
+export { default as Arrow} from '../../../public/weather-icons/arrow.svg'
