68b367e6f0cb3e1289bbef4c272513e25265a542
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-06-21 21:01:29 -0300

    footer fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import MainScreen from "@/pages/screen";
 
 export default function Home() {
   return (
-    <div>
-      <MainScreen/>
-      <Footer></Footer>
+    <div className="flex flex-col h-screen">
+      <div className="flex-grow overflow-y-auto">
+        <MainScreen />
+      </div>
+      <Footer />
     </div>
   );
 }
